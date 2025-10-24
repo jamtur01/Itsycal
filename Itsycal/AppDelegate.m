1bf7f89ece1b6872b50784d8a2943527dce16637
James Turnbulls <james@lovedthanlost.net>
2025-10-23 21:54:12 -0400

    Silenced warning

--- Itsycal/AppDelegate.m
+++ Itsycal/AppDelegate.m
@@ -39,7 +39,8 @@
         kShowEventDots:        @(YES),
         kUseColoredDots:       @(YES),
         kThemePreference:      @0, // System=0, Light=1, Dark=2
-        kHideIcon:             @(NO)
+        kHideIcon:             @(NO),
+        @"TSMLanguageIndicatorEnabled": @(NO) // Suppress Text Services Manager warning
     }];
     
     // Constrain kShowEventDays to values 0...9 in (unlikely) case it is invalid.
